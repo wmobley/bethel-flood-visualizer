4a884cb7d4bd525da45a794e2c2cfec6b374e5b5
Will Mobley <feulmob+github@gmail.com>
2025-07-19 12:27:19 -0500

    Revert "UPdate pages."
    This reverts commit 8af039ac198d102d9b85818083e2b559a52220f8.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/bethel-flood-visualizer/">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   plugins: [react()],
-  base: '/bethel-flood-visualizer/',
-  build: {
-    outDir: 'dist',
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
   },
+  server: {
+    proxy: {
+      '/api/geojson': {
+        target: 'https://web.corral.tacc.utexas.edu',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/geojson/, '/setxuifl/flood-sensor-optimization/water_risk.geojson')
+      }
+    }
+  }
 });
