5a9a2d7518fe28a787cf47734500b26bea9a6abb
Will Mobley <feulmob+github@gmail.com>
2025-07-19 12:31:56 -0500

    Update Vite to handle github pages

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/setxuifl/flood-sensor-optimization/',
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  server: {
-    proxy: {
-      '/api/geojson': {
-        target: 'https://web.corral.tacc.utexas.edu',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/geojson/, '/setxuifl/flood-sensor-optimization/water_risk.geojson')
-      }
-    }
-  }
-});
+  base: '/bethel-flood-visualizer/',
+})
